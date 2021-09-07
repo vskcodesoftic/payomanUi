38d71ab7e002cc8546f81ecbced42320d4aff2fe
vskcodesoftic <dev@codesoftic.com>
2021-09-07 10:18:30 +0530

    added auth pages

---
+++ components/Support/SupportPage.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import Head from 'next/head'
+
+export const SupportPageComponent = () => {
+ 
+   const closeNav = () => {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+ 
+    }
+
+    const openNav = () => {
+        document.getElementById("mySidebar").style.width = "100%";
+        document.getElementById("main").style.marginLeft = "250px";
+      }
+      
+   
+
+    return (
+        <div>
+              <Head>
+        <title>Payoman</title>
+        <meta name="description" content="Siva" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous" />
+
+                        </Head>
+
+                        <div class="navbar-sec p-1" >
+        <div class="container mt-2 mb-2">
+            <div class="row">
+                <div class="col-md-12 nav-link-new font-weight-bold">
+                    <a href="Sidebar"><i class="fa fa-arrow-left mr-3"></i></a>
+                    Support
+                </div>
+            </div>
+        </div>
+    </div>  
+
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+              <img src="images/logo.svg" alt=""  />
+              <p  class="support-content"> Payoman is simply Dummy of the printing and typesetting industry . Payoman as been the industry's standards dummy text ever since the 2011, When an unknown print took a gallery of type andn text it to make a type specimen book . It has survived not only fiv centuries.</p>
+            </div>
+        </div>
+      
+           <div class="row  mt-3">
+               <div class="col-md-12">
+                   <div class=" mt-5 company-list">
+                            <h6 class="" >Company</h6>
+                            <hr class="float-left" />
+                        <ul> 
+                          <div class="mt-5"><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;About Us</a></div>
+                          <div><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Contact Us</a></div>
+                          <div><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Privacy Policy</a></div>
+                          <div><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Support</a></div>
+                          <div><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Terms & Condition</a></div>
+                          <div><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Disclaimer</a></div>
+                      </ul>
+                   </div>
+               </div>
+           </div>
+
+           <div class="row  mt-3">
+               <div class="col-md-12">
+                   <div class=" mt-5 company-list">
+                              <h6 class="" >Social Links</h6>
+                              <hr  class="float-left" />
+                        <ul>
+                           <div class="mt-5"><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Facebook</a></div>
+                           <div><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Twitter</a></div>
+                           <div><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Linkedin</a></div>
+                           <div><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Pinterest</a></div>
+                           <div><a href=""><i class="fa fa-circle fa-sm" ></i> &nbsp;&nbsp;Instagram</a></div>
+                       </ul>
+                   </div>
+               </div>
+           </div>
+
+           <div class="row mt-5 mb-5">
+              <div class="col-md-12 company-list">
+                  <h6 class="">Contact</h6>
+                  <hr />
+                  <div class="mt-5"><a href=""><i class="fa fa-phone fa-sm" ></i> &nbsp;&nbsp;+8572134697</a></div>
+                  <div><a href=""><i class="fa fa-envelope fa-sm" ></i> &nbsp;&nbsp;dummyinf@gmail.com</a></div>
+              </div>
+           </div>
+           <div class="row">
+              <div class="col-md-12">
+                <hr class="float-left" />
+                <p class="text-center">@Copyright PayOman , Design And Develeope By <span>Codesoftic Tech PVT LTD.</span></p>
+              </div>
+           </div>
+        
+     </div>
+  </div>
+    )
+}
+
+export default SupportPageComponent;
--- pages/Privacy.js
+++ pages/Privacy.js
@@ -1,129 +1,28 @@
-import React from 'react'
-import Head from 'next/head'
+import { getSession, useSession } from 'next-auth/client';
+import PrivacyPolicyComponent from '../components/PrivacyAndSecurity/PrivacySecurity';
 
-export const PrivacyPolicy = () => {
- 
-   const closeNav = () => {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
- 
-    }
 
-    const openNav = () => {
-        document.getElementById("mySidebar").style.width = "100%";
-        document.getElementById("main").style.marginLeft = "250px";
-      }
-      
-   
-
-    return (
-        <div>
-              <Head>
-        <title>Payoman</title>
-        <meta name="description" content="Siva" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous" />
-
-                        </Head>
-
-                        <div class="navbar-sec p-1">
-        <div class="container mt-2 mb-2">
-            <div class="row">
-                <div class="col-md-12 nav-link-new font-weight-bold">
-                <a href="Sidebar"><i class="fa fa-arrow-left mr-3"></i></a>
-                    Privacy and Security
-                </div>
-            </div>
-        </div>
-    </div>  
-
-      <div class="container">
-         <div class="row"> 
-             <div class="col-md-12">
-                 <div class="mt-5">
-                     <p>Payoman is simply Dummy of the printing and typesetting industry . Payoman as been the industry's standards dummy text ever since the 2011, When an unknown print took a gallery of type andn text it to make a type specimen book . It has survived not only fiv centuries.</p>
-                  </div>
-             </div>
-         </div>
-         <div class="row mt-4">
-             <div class="col-md-12">
-                 <div class="privacy-sections">
-                    <h4><a href="Privacy.html">Information we collect</a></h4>
-                    <p>We might collect your name, email, mobile number, phone number, street, city, state, pincode, country and Ip address.</p>
-                 </div>
-             </div>
-         </div>
-         <div class="row mt-4">
-               <div class="col-md-12">
-                   <div class="">
-                       <div class="privacy-sections">
-                          <h4><a href="Privacy.html">Payment and Billing Information</a></h4>
-                          <p>We might collect your billing name, billing address and payment method when you buy a ticket. We never collect your credit card number or credit card expiry date or other details pertaining to your credit card on our website. Credit card information will be obtained and processed by our online payment partner CC Avenue.</p>
-                       </div>
-                  </div>
-              </div>
-         </div>
-          <div class="row mt-4">
-              <div class="col-md-12">
-                   <div class="">
-                       <div class="privacy-sections">
-                          <h4><a href="Privacy.html">Information you post</a></h4>
-                          <p>We collect information you post in a public space on our website or on a third-party social media site belonging to Codesoftic.com.</p>
-                      </div>
-                   </div>
-              </div>
-          </div>
-          <div class="row mt-4">
-               <div class="col-md-12">
-                    <div class="">
-                       <div class="privacy-sections">
-                           <h4><a href="Privacy.html">Email Opt-Out</a></h4>
-                           <p>You can opt out of receiving our marketing emails. To stop receiving our promotional emails, please email unsubscriber@codesoftic.com. It may take about  ten days to process your request. Even if you opt out of getting marketing messages, we will still be sending you transactional messages through email and SMS about your purchases.</p>
-                       </div>
-                    </div>
-               </div>
-           </div>
-           <div class="row mt-4">
-            <div class="col-md-12">
-                 <div class="">
-                    <div class="privacy-sections">
-                        <h4><a href="Privacy.html">Third party sites</a></h4>
-                        <p>If you click on one of the links to third party websites, you may be taken to websites we do not control. This policy does not apply to the privacy practices of those websites. Read the privacy policy of other websites carefully. We are not responsible for these third party sites.</p>
-                    </div>
-                 </div>
-            </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col-md-12">
-                 <div class="">
-                    <div class="privacy-sections">
-                        <h4><a href="Privacy.html">Updates to this policy</a></h4>
-                        <p>This Privacy Policy was last updated on 12.12.2014. From time to time we may change our privacy practices. We will notify you of any material changes to this policy as required by law. We will also post an updated copy on our website. Please check our site periodically for updates.</p>
-                    </div>
-                 </div>
-            </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col-md-12">
-                 <div class="">
-                    <div class="privacy-sections">
-                        <h4><a href="Privacy.html">Jurisdiction</a></h4>
-                        <p>If you choose to visit the website, your visit and any dispute over privacy is subject to this Policy and the website’s terms of use. In addition to the foregoing, any disputes arising under this Policy shall be governed by the laws of India.</p>
-                    </div>
-                 </div>
-            </div>
-        </div>
-      </div>
-  
-  </div>
-    )
+function PrivacyPage() {
+  return <PrivacyPolicyComponent />;
 }
 
-export default PrivacyPolicy;
+
+export  async function getServerSideProps(context){
+ const session = await getSession({ req : context.req })
+
+ if(!session) {
+   return {
+     
+     redirect : {
+       destination : '/auth',
+       permanent : false,
+     }
+    }; 
+ }
+  return {
+    props : { session }
+  }
+}
+
+export default PrivacyPage;
+
